81bd6e019f74e1d481c2eb1c88cd78b17696bc0e
Shahjhan-2k19 <56908544+Shahjhan-2k19@users.noreply.github.com>
2022-12-19 22:24:44 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./assets/css/style.css";
+import Note from "./components/note/note";
+
+function App() {
+  return (
+    <div className="App">
+      <Note />
+    </div>
+  );
+}
+
+export default App;
